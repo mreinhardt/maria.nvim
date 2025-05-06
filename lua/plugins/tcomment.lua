178c9ced0c3e78c404f4bbf89916be1afcb4424c
Mike Reinhardt <mreinhardt@riotgames.com>
2025-05-05 18:23:56 -0700

    custom: add TComment plugin

---
+++ lua/plugins/tcomment.lua
@@ -0,0 +1,7 @@
+return {
+    'tomtom/tcomment_vim',
+    keys = {
+        { '<Leader>__', ':TComment<CR>', mode = { 'n' }, 'Toggle commented block' },
+        { '<Leader>--', ':TComment<CR>', mode = { 'n' }, 'Toggle commented block' },
+    },
+}
